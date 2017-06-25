d1849ffe58ac3721bb7b4fe47180d1093354cae2
TobyoTenma <tmtbo@hotmail.com>
2017-06-25 15:29:40 +0800

    fixed: fix some warning

--- TTAImagePickerController/Classes/AssetPicker/TTAAsset.swift
+++ TTAImagePickerController/Classes/AssetPicker/TTAAsset.swift
@@ -22,23 +22,22 @@ public class TTAAsset {
     init(originalAsset: PHAsset) {
         self.originalAsset = originalAsset
     }
-
 }
 
 // MARK: - Request image
 
 extension TTAAsset {
     
-    func requestThumbnail(for size: CGSize, resultHandler: @escaping (UIImage?) -> Void) {
-        if thumbnail != nil { resultHandler(thumbnail); return }
+    func requestThumbnail(for size: CGSize, resultHandler: ((UIImage?) -> Void)?) {
+        if thumbnail != nil { resultHandler?(thumbnail); return }
         return request(for: originalAsset, size: size, options: nil) { [weak self] (image, _) in
-//            self?.thumbnail = image
-            resultHandler(image)
+            self?.thumbnail = image
+            resultHandler?(image)
         }
     }
     
-    func requestOriginalImage(isSync: Bool, resultHandler: @escaping (UIImage?) -> Void) {
-        if originalImage != nil { resultHandler(originalImage); return }
+    func requestOriginalImage(isSync: Bool, resultHandler: ((UIImage?) -> Void)?) {
+        if originalImage != nil { resultHandler?(originalImage); return }
         
         let options = PHImageRequestOptions()
         options.deliveryMode = .highQualityFormat
@@ -47,15 +46,16 @@ extension TTAAsset {
         
         request(for: originalAsset, size: UIScreen.main.bounds.size, options: options) { [weak self] (image, _) in
             self?.originalImage = image
-            resultHandler(image)
+            resultHandler?(image)
         }
     }
     
     private func request(for asset: PHAsset,
-                          size: CGSize,
-                          contentMode: PHImageContentMode? = TTAImagePickerManager.AssetManagerConst.assetMode,
-                          options: PHImageRequestOptions?,
-                          resultHandler: @escaping (UIImage?, [AnyHashable : Any]?) -> Void) {
+                         size: CGSize,
+                         contentMode: PHImageContentMode? = TTAImagePickerManager.AssetManagerConst.assetMode,
+                         options: PHImageRequestOptions?,
+                         resultHandler: @escaping (UIImage?, [AnyHashable : Any]?) -> Void) {
         TTAImagePickerManager.fetchImage(for: self, size: size, contentMode: contentMode, options: options, resultHandler: resultHandler)
+        
     }
 }
