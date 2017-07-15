9591a8cf87d2c083ebc61eaab5b9d8bdc32ca58e
TobyoTenma <tmtbo@hotmail.com>
2017-07-15 16:31:24 +0800

    changed: add the ability for the nearIndexPath contain the current indexPath or not

--- TTAImagePickerController/Classes/Utils/UICollectionView+Extension.swift
+++ TTAImagePickerController/Classes/Utils/UICollectionView+Extension.swift
@@ -10,11 +10,15 @@ import UIKit
 extension UICollectionView {
     
     /// Find the near IndexPaths around `cell` in `section` with `sideCount` pre side
-    func nearIndexPaths(for cell: UICollectionViewCell, in section: Int, sideCount: Int) -> [IndexPath] {
+    /// `isContainCurrent` default is `false`, if `true` the result while incloud the current cell indexPath
+    func nearIndexPaths(for cell: UICollectionViewCell, in section: Int, sideCount: Int, isContainCurrent: Bool = false) -> [IndexPath] {
         guard let currentIndexPath = indexPath(for: cell) else { return [] }
         var nearItems = [IndexPath]()
         for offset in -sideCount...sideCount {
             let index = currentIndexPath.item + offset
+            if index == currentIndexPath.item && !isContainCurrent {
+                continue
+            }
             if index >= 0 && index < numberOfItems(inSection: section) {
                 nearItems.append(IndexPath(item: index, section: section))
             }
