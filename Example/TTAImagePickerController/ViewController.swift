73e46d32f3a1a46dd7ccb72b37dad1f30aa13cd2
TobyoTenma <tmtbo@hotmail.com>
2017-07-15 21:41:35 +0800

    added: add display selected preview images in demo

--- Example/TTAImagePickerController/ViewController.swift
+++ Example/TTAImagePickerController/ViewController.swift
@@ -11,21 +11,46 @@ import TTAImagePickerController
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var maxImageCountTextField: UITextField!
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
+    
+    var selectedImages = [UIImage]()
+    var selectedAssets = [TTAAsset]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        URLSession.shared.dataTask(with: URL(string: "https://www.tobyotenma.top/blog")!).resume()
     }
 
     @IBAction func didClickShowImagePickerButton(_ sender: UIButton) {
         let imagePicker = TTAImagePickerController(selectedAsset: [TTAAsset]())
         imagePicker.selectItemTintColor = .red
         imagePicker.delegate = self
+        imagePicker.selectedAsset = selectedAssets
+        imagePicker.maxPickerNum = Int(maxImageCountTextField.text ?? "9") ?? 9
         present(imagePicker, animated: true, completion: nil)
     }
     
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return selectedImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(UICollectionViewCell.self)", for: indexPath)
+        cell.layer.contents = selectedImages[indexPath.item].cgImage
+        cell.layer.contentsScale = UIScreen.main.scale
+        cell.layer.contentsGravity = "resizeAspectFill"
+        return cell
+    }
+}
+
 extension ViewController: TTAImagePickerControllerDelegate {
     func imagePickerController(_ picker: TTAImagePickerController, didFinishPicking images: [UIImage], assets: [TTAAsset]) {
         print("got the images")
+        selectedImages = images
+        selectedAssets = assets
+        imagesCollectionView.reloadData()
     }
 }
