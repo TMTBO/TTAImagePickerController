e3ce993106174e93acc080c38b42d5937dff0f01
TobyoTenma <tmtbo@hotmail.com>
2017-08-20 17:42:25 +0800

    fixed: fix the code famart

--- TTAImagePickerController/Classes/Preview/TTAPreviewViewController.swift
+++ TTAImagePickerController/Classes/Preview/TTAPreviewViewController.swift
@@ -309,10 +309,9 @@ extension TTAPreviewViewController: TTAPreviewToolBarDelegate {
         if previewDelegate != nil {
             fetchImages(with: selected, completionHandler: { [weak self] (images) in
                 guard let `self` = self else { return }
-                self.previewDelegate?.
-                imagePickerController(self,
-                                      didFinishPicking: images,
-                                      assets: self.selected.map { TTAAsset(asset: $0) })
+                self.previewDelegate?.imagePickerController(self,
+                                                            didFinishPicking: images,
+                                                            assets: self.selected.map { TTAAsset(asset: $0) })
                 self.dismiss(animated: true, completion: nil)
             })
         }
